d295ca466c41fe661997235f0b0bf08cdcae62f7
Matthew-Priest <mwpriest08@gmail.com>
2025-09-14 12:11:17 +0100

    pause menu + buttons
    pause menu works, when clicking escape. can exit pause menu by clicking escape on keyboard or pressing resume. menu button changes scene to main menu and for now, quit button just outputs "exiting application".

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,16 +1,53 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public static bool paused = false;
+
+    public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (paused == true)
+            {
+                Resume();
+            }
+            else if (paused == false)
+            {
+                Pause();
+            }
+        }
+        void Resume()
+        {
+            pauseMenuUI.active = false;
+            Time.timeScale = 1f;
+            paused = false;
+        }
+        void Pause()
+        {
+            pauseMenuUI.active = true;
+            Time.timeScale = 0f;
+            paused = true;
+        }
+    }
+    public void Quit()
+    {
+        Debug.Log("exiting application");
+        Application.Quit();
+    }
+    public void menu()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+    public void resume()
+    {
+        pauseMenuUI.active = false;
+        Time.timeScale = 1f;
+        paused = false;
     }
 }
