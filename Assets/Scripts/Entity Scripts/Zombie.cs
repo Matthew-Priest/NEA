af2593a6088f23de96263bcbd45f6f095baf9e2c
Matthew-Priest <mwpriest08@gmail.com>
2025-12-12 21:00:11 +0000

    fixed zombies freezing
    adding an awake method that incollision = false has stopped all freezing

--- Assets/Scripts/Entity Scripts/Zombie.cs
+++ Assets/Scripts/Entity Scripts/Zombie.cs
@@ -14,6 +14,10 @@ public class Zombie : Entity
         }
         
     }
+    private void Awake()
+    {
+        inCollision = false;
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Plant"))
